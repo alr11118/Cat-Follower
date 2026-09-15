b291cf17b753738fc33918935719d84e34d45522
alr11118 <alara.engin2008@gmail.com>
2026-09-15 12:31:54 -0700

    added pretrained cat detection model

--- catdetect.py
+++ catdetect.py
@@ -1,6 +1,8 @@
 import cv2
+from ultralytics import YOLO
+
 # Load the image
-image = cv2.imread("input.jpeg")
+image = cv2.imread("input.jpg")
  
 # Get the original dimensions
 original_height, original_width = image.shape[:2]
@@ -14,9 +16,18 @@ new_height = int(original_height * aspect_ratio)  # Compute height based on aspe
 resized_image = cv2.resize(image, (new_width, new_height))
  
 # Display the resized image
-cv2.imshow("Resized Image", resized_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.imshow("Resized Image", resized_image)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+cv2.imwrite('resizedImage.jpg', resized_image)
 
 # Run the model on it
-# Return Weather cat is there
+
+# Load a pre-trained YOLO model (e.g., YOLO11 small)
+model = YOLO("yolo11s.pt")
+
+# Predict and filter for class 15 (cat)
+results = model.predict("resizedImage.jpg", classes=[15])
+
+# Show or save the results
+results[0].show()
