ca5d6764b37d25ecd053ea2447d9d3e56adece87
alr11118 <alara.engin2008@gmail.com>
2026-09-15 12:37:57 -0700

    tested the model with more cat imeges

--- catdetect.py
+++ catdetect.py
@@ -2,13 +2,13 @@ import cv2
 from ultralytics import YOLO
 
 # Load the image
-image = cv2.imread("input.jpg")
+image = cv2.imread("data/minnak.png")
  
 # Get the original dimensions
 original_height, original_width = image.shape[:2]
  
 # Define new width while maintaining the aspect ratio
-new_width = 300
+new_width = 512
 aspect_ratio = new_width / original_width
 new_height = int(original_height * aspect_ratio)  # Compute height based on aspect ratio
  
@@ -19,15 +19,14 @@ resized_image = cv2.resize(image, (new_width, new_height))
 #cv2.imshow("Resized Image", resized_image)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
-cv2.imwrite('resizedImage.jpg', resized_image)
+cv2.imwrite('data/resizedImage.jpg', resized_image)
 
 # Run the model on it
-
-# Load a pre-trained YOLO model (e.g., YOLO11 small)
+# Load the pre-trained YOLO model
 model = YOLO("yolo11s.pt")
 
 # Predict and filter for class 15 (cat)
-results = model.predict("resizedImage.jpg", classes=[15])
+results = model.predict("data/resizedImage.jpg", classes=[15])
 
 # Show or save the results
 results[0].show()
